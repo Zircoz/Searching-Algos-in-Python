b708dfaf6df348ba51b98f0bc6b697216a18cc49
JayeshGhosh <62005268+JayeshGhosh@users.noreply.github.com>
2020-10-02 17:14:51 +0530

    Update exponentialsearch.py

--- exponentialsearch.py
+++ exponentialsearch.py
@@ -3,8 +3,8 @@ Explanation:
 Exponential search make use of linear search and Binary search approach
 
 Step1: Finding the range of element we have entered
-Step2:Using binary search to find the index
-Consider a sorted array, and an element x to be searched, find position of x in the array.
+Step2: Using binary search to find the index
+
 
 So if,
 
@@ -14,16 +14,21 @@ Output will be: Element found at index 3
 
 Here Time Complexity = O(Log n)
 '''
-
+#Enter the range of values
 list1=list(map(int, input().split(" ")))
 val=int(input())
 if list1[0] == val:
     print("0")
 i = 1
+
 #Finding range for binarySearch
 while(i<len(list1) and list1[i]<=val):
         i = i * 2
 min1=min(i,len(list1))
+
+#Perform Binary search
+
+#finding the elements from low to high
 def binarySearch(data_list,low,high,value):
     if(high>= low):
         mid=int(low + ( high-low )//2)
@@ -35,7 +40,8 @@ def binarySearch(data_list,low,high,value):
             return binarySearch(data_list,mid + 1,high,value)
     if(high<low):
         return -1
-    # Applying binary search for specified range
+
+#binary search logic from driver code
 index=binarySearch(list1,i/2,min(i,len(list1)),val)
 if(index==-1):
     print("Element not found")
